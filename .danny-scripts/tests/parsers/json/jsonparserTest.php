40399e715e81021909c48d0a29e24565bce42129
Danny Cain <danny@alberon.co.uk>
2019-02-01 15:07:47 +0000

    fixed class-scanner

--- .danny-scripts/tests/parsers/json/jsonparserTest.php
+++ .danny-scripts/tests/parsers/json/jsonparserTest.php
@@ -5,46 +5,46 @@ use Danny\Scripts\Parsers\JSON\Listeners\JSONToArrayListener;
 use PHPUnit\Framework\TestCase;
 
 class JSONParserTest extends TestCase {
-	public function testSimpleArray() {
-		$listener = new JSONToArrayListener();
-		$parser = new JSONParser($listener);
+    public function testSimpleArray() {
+        $listener = new JSONToArrayListener();
+        $parser = new JSONParser($listener);
 
-		$parser->parse('["value 1", "value 2"]');
+        $parser->parse('["value 1", "value 2"]');
 
-		$this->assertTrue($parser->isComplete());
-		$this->assertEquals(["value 1", "value 2"], $listener->get());
-	}
+        $this->assertTrue($parser->isComplete());
+        $this->assertEquals(["value 1", "value 2"], $listener->get());
+    }
 
-	public function testSimpleObject() {
-		$listener = new JSONToArrayListener();
-		$parser = new JSONParser($listener);
+    public function testSimpleObject() {
+        $listener = new JSONToArrayListener();
+        $parser = new JSONParser($listener);
 
-		$parser->parse('{"key" : "value", "key 2" : "value 2"}');
+        $parser->parse('{"key" : "value", "key 2" : "value 2"}');
 
-		$this->assertTrue($parser->isComplete());
-		$this->assertEquals(["key" => "value", "key 2" => "value 2"], $listener->get());
-	}
+        $this->assertTrue($parser->isComplete());
+        $this->assertEquals(["key" => "value", "key 2" => "value 2"], $listener->get());
+    }
 
-	public function testIncompleteJSON_IsNotComplete() {
-		$listener = new JSONToArrayListener();
-		$parser = new JSONParser($listener);
+    public function testIncompleteJSON_IsNotComplete() {
+        $listener = new JSONToArrayListener();
+        $parser = new JSONParser($listener);
 
-		$parser->parse('[ { "id" : 1 }, { "id" : 2 }, { "id" : 3 }');
+        $parser->parse('[ { "id" : 1 }, { "id" : 2 }, { "id" : 3 }');
 
-		$this->assertFalse($parser->isComplete());
-	}
+        $this->assertFalse($parser->isComplete());
+    }
 
-	public function testNestedObject() {
-		$listener = new JSONToArrayListener();
-		$parser = new JSONParser($listener);
+    public function testNestedObject() {
+        $listener = new JSONToArrayListener();
+        $parser = new JSONParser($listener);
 
-		$parser->parse('[ { "id" : 1, "rights" : null, "valid" : false }, { "id" : 2, "rights" : null, "valid" : true }, { "id" : 3, "rights" : null, "valid" : false } ]');
+        $parser->parse('[ { "id" : 1, "rights" : null, "valid" : false }, { "id" : 2, "rights" : null, "valid" : true }, { "id" : 3, "rights" : null, "valid" : false } ]');
 
-		$this->assertTrue($parser->isComplete());
-		$this->assertEquals([
-			[ "id" => 1, "rights" => null, "valid" => false ],
-			[ "id" => 2, "rights" => null, "valid" => true ],
-			[ "id" => 3, "rights" => null, "valid" => false ],
-		], $listener->get());
-	}
+        $this->assertTrue($parser->isComplete());
+        $this->assertEquals([
+            [ "id" => 1, "rights" => null, "valid" => false ],
+            [ "id" => 2, "rights" => null, "valid" => true ],
+            [ "id" => 3, "rights" => null, "valid" => false ],
+        ], $listener->get());
+    }
 }
